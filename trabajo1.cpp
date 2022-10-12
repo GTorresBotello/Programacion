c76086ed241bfac612e150396f89363917752566
Windows_VUbuntu_Torre <gtorresbotello@gmail.com>
2022-10-11 23:09:02 -0500

    Trabajo1

---
+++ trabajo1.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+
+using namespace std;
+
+void funcion6();
+void funcion7();
+
+int main()
+{
+    funcion6();
+    funcion7();
+
+   return 0;
+}
+
+//1
+//
+//2
+//	1) obtenerLargo(),
+//	2) obtenerAncho(),
+//	3) calcArea(),
+//	4) mostrarArea()
+//
+//3
+//	a)
+//	  1.-obtiene la longitud de aluna cosa
+//	  2.-Obtiene el ancho de alguna cosa
+//	  3.-Calcula el area de alguna superficie
+//	  4.-Muestra el area obtenida previamente
+//	b) Se deberian ejecutar en el orden mostrado
+//
+//4
+//	1.-velocidad(): Calcula la velocidad de algo
+//	2.-distacia(): Mide una distacia
+//	3.-aceleracion(): Calcula la aceleracion de algo	
+//
+//5
+//	a)numProm()
+//	b)areaRectangulo()
+//
+//6
+//	funcion6();
+//
+//7
+//	funcion7();
+
+
+void funcion6()
+{
+    cout<<"\nGabino Torres Botello \nMorelia Michoacan \n58000";
+}
+
+void funcion7()
+{
+    cout<<"\n\nLa cosecante de un anggulo \n\tes igual a uno dividido entre \n\t\tel seno del angulo.\n\n";
+}
