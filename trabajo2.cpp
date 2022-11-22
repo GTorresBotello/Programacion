25ee488db62516200d0bd87285235c34a1ea403a
Windows_VUbuntu_Torre <gtorresbotello@gmail.com>
2022-11-21 20:29:19 -0600

    Trabajo 2 added

---
+++ trabajo2.cpp
@@ -0,0 +1,112 @@
+#include<iostream>
+
+using namespace std;
+
+void ejercicio2a();
+void ejercicio2b();
+void ejercicio2c();
+void ejercicio2d();
+void nestedIf ();
+
+int main()
+{
+    //ejercicio2a();
+    //ejercicio2b();
+    //ejercicio2c();
+    //ejercicio2d();
+    nestedIf ();
+
+    return 0;
+}
+
+void ejercicio2a()
+{
+    int as, a, suma, conteo;
+
+    cout << "\nAsigne un valor para :\n    as : ";
+    cin >> as;
+    cout << "\n    a : ";
+    cin >> a;
+
+    if (as < 25)
+	suma = suma + a;
+    else
+	conteo = conteo + 1;
+
+    cout << "\nEl valor de suma es : " << suma
+	 << "\nEl valor de conteo es : " << conteo
+	 << "\n";
+}
+
+void ejercicio2b()
+{
+    int c, voltios, pot;
+    
+    cout << "\nIngrese un valor para c : ";
+    cin >> c;
+
+    if (c == 15){
+	voltios = 5;
+	pot = 10;
+    }else {
+	voltios = 16;
+	pot = 25;
+    }
+
+    cout << "\nel valor de \nvoltaje es : " << voltios
+	 << "\npot es :" << pot
+	 << "\n";
+}
+
+void ejercicio2c()
+{
+    int id, factor = 0;
+    cout << "\nIngrese un valor para id : ";
+    cin >> id;
+
+    if (id > 22)
+	factor = .7;
+    
+    cout << "\nEl valor de el factor es : " << factor
+	 << "\n";
+}
+
+void ejercicio2d()
+{
+    int conteo, promedio, suma;
+    cout << "\nIngrese un valor para conteo : ";
+    cin >> conteo;
+
+    if (conteo == 10){
+	
+	cout << "\nIngrese un valor para suma : ";
+	cin >> suma;
+
+	promedio = suma / conteo;
+	cout << promedio;
+    }
+
+}
+
+void nestedIf ()
+{
+    char grado;
+    int peso, bin, t, s, a;
+    
+    cout << "\nIngrese el valor para grado : ";
+    cin >> grado;
+
+    cout << grado;
+
+    if (grado == 'A')
+	if (peso > 35){
+	    bin = 1
+	    t = s + a;
+	}
+
+    int suma = 0, conteo, grado, fallo;
+
+    if (conteo < 5)
+	if (grado < 50)
+	    fallo = fallo + 1;
+}
